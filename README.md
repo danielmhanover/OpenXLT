5246f3d1fb427cb246e482ba90e717556d508391
Daniel <hanover.daniel@hotmail.com>
2014-01-13 20:13:20 -0500

    Added Source Files

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
+**NOTICE: There are known bugs that may cause OpenXLT to incorrectly convert XLTek data at this time. We are actively working to fix these issues.**
+
 OpenXLT
 =======
+*OpenXLT* is a free, open-source C++ program for the conversion of EEG data from the Natus XLTek file format to formats readable by other programs. Currently, only the .mat file format is supported for exporting.
+
+### Usage
+For the packaged executables to work correctly, your system must have Matlab installed and the proper Environment variables set. See [this page](http://www.mathworks.com/help/matlab/matlab_external/compiling-engine-applications-with-the-mex-command.html) for details.
+
+On a UNIX system, execute the following:
+```
+$ ./main <dirname>
+```
+where dirname contains the XLTek binary files to convert. Currently, the dirname must have a trailing slash or you will get an error.
+
+On Windows, open a command prompt and CD into the directory with your binary and execute:
+```
+main.exe <dirname>
+```
+In this case, the dirname must NOT have a trailing backslash. This limitation will be fixed in future versions.
+
+### Support
+Having trouble with OpenXLT? File a bug report.
---
+++ src/Unix/openxlt.cpp
@@ -0,0 +1,230 @@
+//
+//  openxlt.cpp
+//
+//  Created by Daniel Hanover on 7/22/13.
+//  Copyright (c) 2013 klab. All rights reserved.
+//
+
+#include <iostream>
+#include <fstream>
+#include <libgen.h>
+#include <string>
+#include <regex.h>
+#include <ctime>
+#include <stdio.h>
+#include <string.h>
+#include "mat.h"
+
+using namespace std;
+
+#define SECS_PER_DAY 86400
+time_t serialConvert(long double s) { /* convert day number to y,m,d format */
+    time_t rawtime;
+    struct tm * timeinfo = new tm();
+    long d = s/(int)1;
+    long double p = s - d;
+    
+    // Modified Julian to DMY calculation with an addition of 2415019
+    int l = d + 68569 + 2415019;
+    int n = int(( 4 * l ) / 146097);
+    l = l - int(( 146097 * n + 3 ) / 4);
+    int i = int(( 4000 * ( l + 1 ) ) / 1461001);
+    l = l - int(( 1461 * i ) / 4) + 31;
+    int j = int(( 80 * l ) / 2447);
+    timeinfo->tm_mday = l - int(( 2447 * j ) / 80);
+    l = int(j / 11);
+    timeinfo->tm_mon = j + 2 - ( 12 * l );
+    timeinfo->tm_year = 100 * ( n - 49 ) + i + l - 1900;
+    
+    rawtime = mktime(timeinfo);
+    rawtime += SECS_PER_DAY*p - 3600;
+    return rawtime;
+}
+
+double roundTenths(double v) {
+    v = (int)(v*10 + 0.5);
+    return v/10;
+}
+
+int fpeek(FILE *stream)
+{
+    int c;
+    c = fgetc(stream);
+    ungetc(c, stream);
+    return c;
+}
+
+//Read First Sample Number from ETC File
+int getSampleNumberFromETC(string path, string filename) {
+    int sampleNumber;
+    //Load ETC file
+    FILE *etcf = fopen((path + filename + string(".etc")).c_str(), "r");
+    fseek(etcf, 356, SEEK_SET);
+    fread(&sampleNumber, 4, 1, etcf);
+    fclose(etcf);
+    return sampleNumber;
+}
+
+#define COLUMNS 130
+int main(int argc, const char * argv[])
+{
+    //Variables
+    int samplingFrequency;
+    double creationSeconds;
+    int sampleNumber = 0;
+    int initSampleNumber;
+    string filename = "";
+    
+    //Transfer data in EEG file to string
+    string path(argv[1]);
+    string name(basename(const_cast<char*>(argv[1])));
+    cout << (path + name) << endl;
+    FILE *eegf = fopen((path + name + string(".eeg")).c_str(), "r");
+    if(!eegf) {
+        cout << "Failed to open EEG file for read. Terminating NOW" << endl;
+        return 1;
+    }
+    fseek(eegf, 0, SEEK_END);
+    long eegsize = ftell(eegf);
+    fseek(eegf, 0, SEEK_SET);
+    char *eegcontents;
+    eegcontents = new char[eegsize];
+    
+    char x;
+    while (!feof(eegf)) {
+        if((x = fgetc(eegf)) != '\0') {
+            eegcontents[strlen(eegcontents)] = x;
+        }
+    }
+    fclose(eegf);
+    
+    //Parse EEG data for sampling frequency and Creation Time
+    regex_t regex;
+    regmatch_t match[2];
+    int reti;
+    reti = regcomp(&regex, "\\(\\.\"SamplingFreq\", ([0-9]+\\.[0-9]+)\\)", REG_EXTENDED);
+    reti = regexec(&regex, eegcontents, 3, match, REG_NOTBOL);
+    char *sambuf = new char[match[1].rm_eo-match[1].rm_so];
+    strncpy(sambuf, eegcontents+match[1].rm_so, match[1].rm_eo-match[1].rm_so);
+    samplingFrequency = atoi(sambuf);
+    regfree(&regex);
+    
+    reti = regcomp(&regex, "\\(\\.\"CreationTime\", ([0-9]+\\.[0-9]+)\\)", REG_EXTENDED);
+    reti = regexec(&regex, eegcontents, 3, match, REG_NOTBOL);
+    sambuf = new char[match[1].rm_eo-match[1].rm_so];
+    strncpy(sambuf, eegcontents+match[1].rm_so, match[1].rm_eo-match[1].rm_so);
+    creationSeconds = serialConvert(atof(sambuf));
+    regfree(&regex);
+    
+    //Repeatedly parse STC file for duration and file name data
+    FILE *stcf = fopen((path + name + string(".stc")).c_str(), "r");
+    if(!stcf) {
+        cout << "Failed to open STC file for read. Terminating NOW" << endl;
+        return 1;
+    }
+    fseek(stcf, 408, SEEK_SET);
+    
+    char f;
+    int c = 0;
+    while ((f = fgetc(stcf)) != EOF) {
+        //Read filename and duration from STC file
+        filename = "";
+        int d;
+        do {
+            filename += f;
+        } while((f = fgetc(stcf)) != '\x00');
+        
+        fseek(stcf, 267 - filename.length(), SEEK_CUR);
+        fread(&d, 4, 1, stcf);
+        double duration = d*0.000499963760375975;
+        
+        cout << "Processing " << filename << ".erd" << endl;
+        
+        //Load ERD file
+        FILE *erdf = fopen((path + filename + string(".erd")).c_str(), "r");
+        
+        //Read current sample number from ETC file
+        if(sampleNumber == 0) {
+            sampleNumber = getSampleNumberFromETC(path, filename);
+        }
+        initSampleNumber = sampleNumber;
+        fseek(erdf, 8656, SEEK_SET);
+        
+        mxArray *data = mxCreateDoubleMatrix(COLUMNS, (int)(duration*2100), mxREAL);
+        double *dataptr = mxGetPr(data);
+        
+        //Read data from ERD File
+        char peek;
+        while ((peek = fpeek(erdf)) != EOF || !feof(erdf)) {
+            
+            int sampleOffset = sampleNumber - initSampleNumber;
+			if (sampleNumber + 1 % samplingFrequency == 0) {
+				creationSeconds++;
+			}
+			//Timestamp
+			dataptr[sampleOffset*COLUMNS] = creationSeconds;
+			//Sample Number
+			dataptr[sampleOffset*COLUMNS + 1] = sampleNumber;
+			fseek(erdf, 1, SEEK_CUR);
+			//Read delta voltage data from ERD file
+			unsigned char groupnumrep[16] = {};
+			unsigned char redoindices[16] = {};
+			fread(&groupnumrep, 1, 16, erdf);
+			for (size_t i = 0; i < 128; i++) {
+				double dvolt;
+				if (groupnumrep[i / 8] % 2 == 1) {
+					//Must account for varying endianness... Ugh!
+					unsigned char buf[2];
+					fread(&buf, 1, 2, erdf);
+					signed short volt = (buf[0]) | (buf[1] << 8);
+					dvolt = volt*0.265839;
+					if (volt == -1) {
+						redoindices[i / 8] += (1 << i % 8);
+					}
+				}
+				else {
+					signed char volt;
+					fread(&volt, 1, 1, erdf);
+					dvolt = volt*0.265839;
+				}
+				groupnumrep[i / 8] >>= 1;
+				//dvolt = roundTenths(dvolt);
+				dataptr[sampleOffset*COLUMNS + i + 2] = dataptr[(sampleOffset - 1)*COLUMNS + i + 2] + dvolt;
+                
+			}
+			for (size_t i = 0; i < 128; i++)
+			{
+				if (redoindices[i / 8] % 2 == 1) {
+					int volt = 0;
+					fread(&volt, 4, 1, erdf);
+					double dvolt = volt*0.265839;
+					//dvolt = roundTenths(dvolt);
+					dataptr[sampleOffset*COLUMNS + i + 2] = dvolt;
+				}
+				redoindices[i / 8] >>= 1;
+			}
+			sampleNumber++;
+        }
+        
+        //Close ERD file
+        fclose(erdf);
+        char varname[11];
+        sprintf(varname, "Out%.3d.mat", c);
+        
+        //Create MATLAB File
+        MATFile *pmat;
+        pmat = matOpen(varname, "w");
+        
+        int status = matPutVariable(pmat, "EEGData", data);
+        if(status != 0) {
+            cout << "Error " << status << " writing data to MAT File. Terminating NOW";
+            return 1;
+        }
+        mxDestroyArray(data);
+        matClose(pmat);
+        c++;
+    }
+    //Close STC File
+    fclose(stcf);
+    return 0;
+}
